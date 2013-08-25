bf2981b5b9688730eb70dc8dfe2a3decdc692cff
Bryan Zarzuela <bzarzuela@gmail.com>
2013-08-25 12:43:15 +0800

    Checking number of player argument

--- poker.php
+++ poker.php
@@ -4,7 +4,11 @@ spl_autoload_register(function ($class) {
   include 'classes/' . $class . '.class.php';
 });
 
-$players = isset($_SERVER['argv'][1]) ? $_SERVER['argv'] : rand(2, 6);
+$players = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : rand(2, 6);
+
+if ($players < 2 or $players > 6) {
+  die('Number of players must be between 2 to 6');
+}
 
 $game = new Game;
 $game->setLogger(new Logger);
